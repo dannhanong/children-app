ba84e0f72ddbd0e2cb4e194020e1de827ab85820
dannhanong <thannong0512@gmail.com>
2025-05-31 21:49:10 +0700

    update miss 14

--- src/main/java/com/team/child_be/models/ChildBattery.java
+++ src/main/java/com/team/child_be/models/ChildBattery.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -28,7 +29,7 @@ public class ChildBattery {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
-    @OneToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "child_id")
     User child;
 
